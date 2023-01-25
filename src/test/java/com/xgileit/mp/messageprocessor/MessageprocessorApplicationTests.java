ba7999698750af7239cba5fa4e3fc5088ade8ab6
Rupesh <rupeshgaudel3@gmail.com>
2023-01-25 19:03:37 +0545

    code base for message processor

---
+++ src/test/java/com/xgileit/mp/messageprocessor/MessageprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xgileit.mp.messageprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageprocessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
