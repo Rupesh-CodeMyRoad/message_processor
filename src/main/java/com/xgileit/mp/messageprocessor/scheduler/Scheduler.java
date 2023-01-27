d16f5ec19353702c8178dd3a24f867f01609b9cc
Rupesh <rupeshgaudel3@gmail.com>
2023-01-27 12:16:40 +0545

    Message Processor - Scheduler

--- src/main/java/com/xgileit/mp/messageprocessor/scheduler/Scheduler.java
+++ src/main/java/com/xgileit/mp/messageprocessor/scheduler/Scheduler.java
@@ -1,14 +1,12 @@
-package com.xgileit.mp.messageprocessor.scheduler;
-
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-@Component
-public class Scheduler {
-    @Scheduled(cron = "59 23 * * *")
-    public void cronJobSch() {
-        System.out.println("hello");
-    }
-}
+//package com.xgileit.mp.messageprocessor.scheduler;
+//
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class Scheduler {
+//    @Scheduled(cron = "59 23 * * *")
+//    public void cronJobSch() {
+//        System.out.println("hello");
+//    }
+//}
