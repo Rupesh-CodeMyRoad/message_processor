74f50724aa8d6160b3517a7cc468a163586311e3
Tebogo Assagaai <tebogo.assagaai@xgileit.com>
2023-01-30 12:05:07 +0200

    changes

--- src/main/java/com/xgileit/mp/messageprocessor/scheduler/Scheduler.java
+++ src/main/java/com/xgileit/mp/messageprocessor/scheduler/Scheduler.java
@@ -1,13 +1,16 @@
 package com.xgileit.mp.messageprocessor.scheduler;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xgileit.mp.messageprocessor.controller.MessageParserController;
 import com.xgileit.mp.messageprocessor.dto.RequestDto;
 import com.xgileit.mp.messageprocessor.model.RequestResponse;
 import com.xgileit.mp.messageprocessor.repository.RequestResponseRepo;
@@ -16,13 +19,17 @@ import com.xgileit.mp.messageprocessor.repository.RequestResponseRepo;
 public class Scheduler {
     
 	@Autowired RequestResponseRepo requestResponseRepo;
-	@Scheduled(cron = "* 23 10 * * *")
-    public void cronJobSch() {
-      
-        System.out.println("hello");
-  List<RequestResponse> requestList = requestResponseRepo.findAll().stream().filter(requestResponse ->Boolean.FALSE==requestResponse.getStatus()).collect(Collectors.toList());
+	
+	@Autowired MessageParserController  messageParserController;
+	
+	@Scheduled(cron = "0 0 12 * * ?")
+    public void cronJobSch() throws JsonMappingException, JsonProcessingException {
+    List<RequestResponse> requestList = requestResponseRepo.findAll().stream().filter(requestResponse ->Boolean.FALSE==requestResponse.getStatus()).collect(Collectors.toList());
     for (RequestResponse request : requestList) {
-		
+	
+    RequestDto reDto	= new ObjectMapper().readValue(request.getRequest(), RequestDto.class);  
+    messageParserController.saveSubType(reDto);
+     System.out.println("request :"+reDto);
 	}
 	}
 }
